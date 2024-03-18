cf2fb664eb56a6520ed3c6c2d8a067c81c2e79f7
Sathvik Bhavaraju <sathvikb21@gmail.com>
2024-03-18 08:33:03 -0400

    removed navlinks for prod

--- src/components/Menu/MenuLinks.jsx
+++ src/components/Menu/MenuLinks.jsx
@@ -7,11 +7,11 @@ function MenuLinks() {
         <nav className="px-5 h-full flex flex-col">
             <ActiveLink to="/learn/" label="Dashboard" />
             <ActiveLink to="/learn/lessons" label="Lessons" />
-            <ActiveLink to="#" label="Messages" />
+            {/* <ActiveLink to="#" label="Messages" />
             <ActiveLink to="#" label="News" />
             <ActiveLink to="#" label="Quiz" />
             <ActiveLink to="/trade/simulation" label="Trade Simulation" />
-            <ActiveLink to="#" label="Settings" classNames="mt-auto" />
+            <ActiveLink to="#" label="Settings" classNames="mt-auto" /> */}
             <button onClick={logout} className="appearance-none">
                 <ActiveLink to="" label="Logout" />
             </button>
