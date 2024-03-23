4087bc87889622c40c4073e89e275a6f85878dea
Colby <colby.b.cooper@gmail.com>
2024-03-23 13:43:26 -0400

    Update privacy

--- src/pages/Privacy.jsx
+++ src/pages/Privacy.jsx
@@ -7,7 +7,7 @@ export default function Privacy() {
             <main className="flex flex-col max-w-screen w-full overflow-x-hidden dark:bg-[#0F0F0F] dark:text-[#FBFBFB]">
                 <LandingNav />
 
-                <div className="w-full container mx-auto p-8 md:p-4 my-12 flex items-start gap-8">
+                <div className="w-full container max-w-screen-lg mx-auto p-8 md:p-4 my-12 flex flex-col md:flw-row items-start gap-8">
                     <div className="w-full md:w-1/3 border border-gray-200 dark:border-gray-700 rounded-md p-4">
                         <ul className="list-none">
                             <li>
